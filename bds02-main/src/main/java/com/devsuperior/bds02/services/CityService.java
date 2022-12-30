5491d8386137f39ecb7c58067c413ef1066147a4
victor moraes <victorhardflipmoraes@outlook.com>
2022-12-30 16:17:07 -0400

    Insert implemented

--- bds02-main/src/main/java/com/devsuperior/bds02/services/CityService.java
+++ bds02-main/src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -23,4 +23,12 @@ public class CityService {
 		List<City> list = repository.findAll(Sort.by("name"));
 		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
 	}
+	
+	@Transactional
+	public CityDTO insert(CityDTO dto) {
+		City entity = new City();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CityDTO(entity);
+	}
 }
